f66e6c3d772bc153f3e177b925e6c52ba22a2c68
sébastien durand <dragonlost1@hotmail.fr>
2019-05-15 18:58:30 +0200

    MEGA debug !!

--- als.py
+++ als.py
@@ -5,99 +5,151 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from alsui import Ui_stack_window  # import du fichier alsui.py généré par : pyuic5 alsui.ui -x -o alsui.py
+
+# from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import stack as stk
 
 
-import alsui  # import du fichier alsui.py généré par : pyuic5 alsui.ui -x -o alsui.py
+class MyEventHandler(FileSystemEventHandler, QtCore.QThread):
+    created_signal = QtCore.pyqtSignal()
+    new_image_path = ""
+
+    def __init__(self):
+        super(MyEventHandler, self).__init__()
+
+    def on_created(self, event):
+        if not event.is_directory:
+            print("New image arrive: %s" % event.src_path)
+            self.new_image_path = event.src_path
+            self.created_signal.emit()
+
 
 # ------------------------------------------------------------------------------
+
 class WatchOutForFileCreations(QtCore.QThread):
-    def __init__(self, path, filename):
+    def __init__(self, path, work_folder, first_image):
         super(WatchOutForFileCreations, self).__init__()
         self.path = path
-        self.filename = filename
+        self.work_folder = work_folder
+        self.first_image = first_image
         self.observer = Observer()
-        self.event_handler = MyEventHandler(self.filename)
+        self.event_handler = MyEventHandler()
         self.observer.schedule(self.event_handler, self.path, recursive=False)
         self.observer.start()
-        self.connect(self.event_handler, QtCore.SIGNAL("fileCreated"), self.modified)
+        # self.connect(self.event_handler, QtCore.SIGNAL("fileCreated"), self.modified)
+        self.event_handler.created_signal.connect(self.created(self.event_handler.new_image_path))
+        stk.create_first_ref_im(self.work_folder, self.first_image, "stack_ref_image.fit")
 
     def run(self):
         pass
 
-    def created(self):
-        self.emit(QtCore.SIGNAL("fileCreated1"))
+    def created(self, new_image_path):
+        # appelle de la fonction stack live
+        # stk.stack_live(self.work_folder, new_image_path, "stack_ref_image.fit", mode="rgb", save_im=True)
+        print("file created : %s" % new_image_path)
 
 
 # ------------------------------------------------------------------------------
 
 
-class MyWindow(QtWidgets.QMainWindow):
+class als_main_window(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        QtWidgets.QMainWindow.__init__(self, parent)
-        self.ui = alsui.Ui_MainWindow()
+        super(als_main_window, self).__init__(parent)
+        self.ui = Ui_stack_window()
         self.ui.setupUi(self)
-        
+
+        self.connect_actions()
+        self.running = False
+        self.counter = 0
+
+    def connect_actions(self):
+
         self.ui.pbPlay.clicked.connect(self.cb_play)
         self.ui.pbStop.clicked.connect(self.cb_stop)
-        self.ui.pbReset.clicked.connect(self.cb_reset)   
-                     
-        self.ui.bBrowseFolder.clicked.connect(self.cb_browse_folder)                
-        self.ui.bBrowseDark.clicked.connect(self.cb_browse_dark)                
-        
-        self.running = False
-        
-        self.counter=0
+        self.ui.pbReset.clicked.connect(self.cb_reset)
+        self.ui.bBrowseFolder.clicked.connect(self.cb_browse_folder)
+        self.ui.bBrowseDark.clicked.connect(self.cb_browse_dark)
 
-                     
-# ------------------------------------------------------------------------------
-# Callbacks
+    # ------------------------------------------------------------------------------
+    # Callbacks
     def cb_browse_folder(self):
-        DirName = QtWidgets.QFileDialog.getExistingDirectory(self,"Répertoire à scanner",self.ui.tFolder.text())
+        DirName = QtWidgets.QFileDialog.getExistingDirectory(self, "Répertoire à scanner", self.ui.tFolder.text())
         if DirName:
             self.ui.tFolder.setText(DirName)
-    
+            self.ui.pbPlay.setEnabled(True)
+
     def cb_browse_dark(self):
-        fileName,_ = QtWidgets.QFileDialog.getOpenFileName(self,"Fichier de Dark", "","Fit Files (*.fit);;All Files (*)")
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Fichier de Dark", "",
+                                                            "Fit Files (*.fit);;All Files (*)")
         if fileName:
             self.ui.tDark.setText(fileName)
- 
+
     def cb_play(self):
-        # recuperation des parametres
-        self.ui.log.append("Play")
+        if self.ui.tFolder.text() != "":
 
-        if len(self.ui.tFolder.text())==0 :
-            print( "Dossier a scanner: <./>")
-        else: 
-            print( "Dossier a scanner: <" + self.ui.tFolder.text() +">")
-        if self.ui.cbAlign.isChecked():
-            print( "Alignement"  )        
-        if (self.ui.cbDark.isChecked())&(len(self.ui.tDark.text())!=0):
-            print( "Dark : <" + self.ui.tDark.text()  +">")   
-                         
-        self.running = True
-        self.ui.pbPlay.setEnabled(False)
-        self.ui.pbStop.setEnabled(True)
-        self.counter=0
+            # Print scan folder
+            self.ui.log.append("Dossier a scanner: <" + self.ui.tFolder.text() + ">")
 
-        # Lancement du watchdog
-           
+            # check align
+            if self.ui.cbAlign.isChecked():
+                self.align = True
+
+            # check dark
+            if (self.ui.cbDark.isChecked()) & (self.ui.tDark.text() != ""):
+                self.ui.log.append("Dark : <" + self.ui.tDark.text() + ">")
+                self.dark = True
+
+            # Lancement du watchdog
+
+            # /!\ besoin de créer 2 nouvelle zone, un pour le dossier de travail et un pour pointer la première image
+            self.fileWatcher = WatchOutForFileCreations(self.ui.tFolder.text(), work_folder, first_image)
+            self.fileWatcher.start()
+
+            # Print live method
+            if self.align and self.dark:
+                self.ui.log.append("Play with alignement and Dark")
+            elif self.align:
+                self.ui.log.append("Play with alignement")
+            else:
+                self.ui.log.append("Play")
+
+            self.running = True
+
+            # desactivate play button
+            self.ui.pbPlay.setEnabled(False)
+            # activate stop button
+            self.ui.pbStop.setEnabled(True)
+            self.counter = 0
+        else:
+            self.ui.log.append("No have path")
+
     def cb_stop(self):
         self.running = False
+        # /!\ impossible d'arreter le watchdog
+
+        # self.fileWatcher.stop()
+        # Observer.stop(self.fileWatcher)
+        self.counter = 0
+        self.ui.progressBar.setValue(self.counter)
+
         self.ui.pbStop.setEnabled(False)
         self.ui.pbPlay.setEnabled(True)
-        observer.stop()
-        self.counter=0
-        self.ui.progressBar.setValue(self.counter)
-                    
+        self.ui.log.append("Stop")
+
     def cb_reset(self):
-        self.counter=0
-                     
+        self.counter = 0
+
+    def main(self):
+        self.show()
 
 
 # ------------------------------------------------------------------------------
-import sys
-app = QtWidgets.QApplication(sys.argv)
-window = MyWindow()
-window.show()
 
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = als_main_window()
+    window.main()
+    sys.exit(app.exec_())
---
+++ stack.py
@@ -0,0 +1,86 @@
+import os
+import cv2
+import astroalign as al
+import numpy as np
+from astropy.io import fits
+from tqdm import tqdm
+import shutil
+
+
+def create_first_ref_im(work_path, im_path, ref_name):
+    # copy first image in work path
+    shutil.copy2(im_path, work_path + ref_name)
+
+
+def stack_live(work_path, new_image, ref_name, mode="rgb", save_im=True):
+    # search number
+    new_name = new_image.split('_')
+    number = new_name[1].replace('.fit', '')
+
+    # open new image
+    new_fit = fits.open(new_image)
+    new = new_fit[0].data
+    new_fit.close()
+    # search type
+    new_type = new.dtype.name
+    if new_type == 'uint8':
+        new_limit = 255
+    elif new_type == 'uint16':
+        new_limit = 65535
+    else:
+        raise ValueError("format not support")
+
+    # open ref image
+    ref_fit = fits.open(work_path + ref_name)
+    ref = ref_fit[0].data
+    ref_fit.close()
+    # search type
+    ref_type = ref.dtype.name
+    if ref_type == 'uint8':
+        ref_limit = 255
+    elif ref_type == 'uint16':
+        ref_limit = 65535
+    else:
+        raise ValueError("format not support")
+
+    # format verification
+    if ref_limit != new_limit:
+        raise ValueError("ref image and new image is not same format")
+
+    # choix rgb ou gray scale
+    if mode == "rgb":
+        # alignement
+        p, __ = al.find_transform(new[1], ref[1])
+        # stacking
+        stack_image = []
+        for j in tqdm(range(3)):
+            stack_image.append(al.apply_transform(p, new[j], ref[j]) + ref[j])
+            stack_image[j] = np.where(stack_image[j] < ref_limit, stack_image[j], ref_limit)
+
+    elif mode == "gray":
+        # alignement
+        p, __ = al.find_transform(new, ref)
+        # stacking
+        stack_image = [al.apply_transform(p, new, ref) + ref]
+        stack_image = np.where(stack_image < ref_limit, stack_image, ref_limit)
+    else:
+        raise ValueError("Mode not support")
+
+    if save_im:
+        os.remove(work_path + ref_name)
+        # save stack image in fit (for save all)
+        red = fits.PrimaryHDU(data=stack_image)
+        red.writeto(work_path + ref_name)
+
+    # save stack image in fit (new ref image)
+    red = fits.PrimaryHDU(data=stack_image)
+    red.writeto(work_path + "/stack_image_" + number + ".fits")
+
+    # save stack image in tiff (print image)
+    # invert Red and Blue for cv2
+    new_stack_image = np.rollaxis(stack_image, 0, 3)
+    new_stack_image[:, :, 0] = stack_image[2, :, :]
+    new_stack_image[:, :, 2] = stack_image[0, :, :]
+    cv2.imwrite(work_path + "/stack_image_" + number, new_stack_image)
+
+    return "./stack_image_" + number + ".tiff", "./stack_image_" + number + ".fits"
