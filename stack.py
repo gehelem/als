71849d9cb2599a9ca6bc4d75125aa93686bd8290
dragonlost <dragonlost1@hotmail.fr>
2019-06-05 12:25:38 +0200

    add dell stack for memory

--- stack.py
+++ stack.py
@@ -236,14 +236,15 @@ def stack_live(work_path, im_path, counter, ref=[], first_ref=[], save_im=False,
                 stack = np.float32(align_image) + np.float32(ref[j])
             elif stack_methode == "Mean":
                 stack = ((counter - 1) * np.float32(ref[j]) + np.float32(align_image)) / counter
-
+            else:
+                raise ValueError("Stack method is not support")
+                
             # filter excess value > limit
             if im_type == 'uint8':
                 stack_image.append(np.uint8(np.where(stack < 2 ** 8 - 1, stack, 2 ** 8 - 1)))
             elif im_type == 'uint16':
                 stack_image.append(np.uint16(np.where(stack < 2 ** 16 - 1, stack, 2 ** 16 - 1)))
-            else:
-                raise ValueError("Stack method is not support")
+        del stack
         del new
 
     elif im_mode == "gray":
@@ -271,7 +272,8 @@ def stack_live(work_path, im_path, counter, ref=[], first_ref=[], save_im=False,
             stack_image = np.uint8(np.where(stack < 2 ** 8 - 1, stack, 2 ** 8 - 1))
         elif im_type == 'uint16':
             stack_image = np.uint16(np.where(stack < 2 ** 16 - 1, stack, 2 ** 16 - 1))
-
+        del stack
+        
     else:
         raise ValueError("Mode not support")
 
