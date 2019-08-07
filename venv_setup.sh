f1ad13e9161affe9830f9851032ce3cd1478e617
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2019-08-07 18:10:09 -0400

    Automatically create the needed directories

--- venv_setup.sh
+++ venv_setup.sh
@@ -15,14 +15,32 @@
 
 set -e
 
+# Check if we have a requirements file
 if ! [[ -r requirements.txt ]]
 then
     echo "Error : Please make sure you run this script from within the als directory"
     exit 1
 fi
 
+# Update package list
 sudo apt-get update
+
+# Install venv
 sudo apt-get install python3-venv
+
+# Create the venve
 python3 -m venv venv
+
+# Load the needed environment variables
 source venv/bin/activate
+
+# Now install all the needed Python stuff
 pip install -r requirements.txt
+
+# Automatically create the directories
+for DIR in wrk scan
+do
+	if [ ! -d $DIR ]; then
+		mkdir $DIR
+	fi
+done
