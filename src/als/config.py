da5b03f74643c2f60aee0e74eab65f8bce4021b0
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2019-09-08 14:25:37 +0200

    complete docstrings in config.py

--- src/als/config.py
+++ src/als/config.py
@@ -59,14 +59,25 @@ _DEFAULTS = {
 }
 _MAIN_SECTION_NAME = "main"
 
-_config_parser = ConfigParser()
+_CONFIG_PARSER = ConfigParser()
 
 
 def is_debug_log_on():
-    return "DEBUG" == _get(_LOG_LEVEL)
+    """
+    Checks if loglevel is DEBUG
+
+    :return: True if loglevel is DEBUG, False otherwise
+    """
+    return _get(_LOG_LEVEL) == 'DEBUG'
 
 
 def set_debug_log(debug_active):
+    """
+    Sets logldevel to debug if debug_active is True, otherwise set loglevel to info
+
+    :param debug_active: set loglevel to debug
+    :type debug_active: bool
+    """
     if debug_active:
         _set(_LOG_LEVEL, _LOG_LEVEL_DEBUG)
     else:
@@ -74,7 +85,12 @@ def set_debug_log(debug_active):
 
 
 def get_www_server_port_number():
-    # if config value is not an int, we switch to default value
+    """
+    Retrieves the configured web server port number.
+
+    :return: The configured port number, or its default value if config entry
+             is not parsable as an int.
+    """
     try:
         return int(_get(_WWW_SERVER_PORT))
     except ValueError:
@@ -82,53 +98,126 @@ def get_www_server_port_number():
 
 
 def set_www_server_port_number(port_number):
+    """
+    Sets server port number.
+
+    :param port_number: the port number
+    :type port_number: int
+    """
     _set(_WWW_SERVER_PORT, port_number)
 
 
 def get_work_folder_path():
+    """
+    Retrieves work folder path.
+
+    :return: the work folder path
+    :rtype: str
+    """
     return _get(_WORK_FOLDER_PATH)
 
 
 def set_work_folder_path(path):
+    """
+    Sets the work folder path.
+
+    :param path: the work folder path
+    :type path: str
+    """
     _set(_WORK_FOLDER_PATH, path)
 
 
 def get_scan_folder_path():
+    """
+    Retrieves scan folder path.
+
+    :return: the scan folder path
+    :rtype: str
+    """
     return _get(_SCAN_FOLDER_PATH)
 
 
 def set_scan_folder_path(path):
+    """
+    Sets the scan folder path.
+
+    :param path: the scan folder path
+    :type path: str
+    """
     _set(_SCAN_FOLDER_PATH, path)
 
 
 def get_window_geometry():
+    """
+    Retrieves main window geometry.
+
+    :return: a tuple of 4 integers describing :
+
+    - x coordinate of top left corner
+    - y coordinate of top left corner
+    - width of the window
+    - height of the window
+    """
     return tuple([int(value) for value in _get(_WINDOW_GEOMETRY).split(",")])
 
 
 def set_window_geometry(geometry_tuple):
+    """
+    Sets main window geometry.
+
+    :param geometry_tuple:
+    :type geometry_tuple: tuple
+
+    The geometry_tuple tuple must contain 4 integers describing :
+
+    - x coordinate of top left corner
+    - y coordinate of top left corner
+    - width of the window
+    - height of the window
+    """
     _set(_WINDOW_GEOMETRY, ",".join([str(value) for value in geometry_tuple]))
 
 
 def save():
+    """
+    Saves settings to disk.
+
+    :except os_error: Saving could not be done
+    """
     try:
         with open(_CONFIG_FILE_PATH, "w") as config_file:
-            _config_parser.write(config_file)
-        _logger.info("User configuration saved")
+            _CONFIG_PARSER.write(config_file)
+        _LOGGER.info("User configuration saved")
     except OSError as os_error:
-        _logger.error("Could not save settings. Error : %s", os_error)
+        _LOGGER.error("Could not save settings. Error : %s", os_error)
         dialogs.error_box("Settings not saved", f"Your settings could not be saved\n\nDetails : {os_error}")
 
 
 def _get(key):
+    """
+    Retrieves the value of a specific config entry.
+
+    :param key: the key identifying the config entry
+    :type key: str
+
+    :return: the value of the config entry identified by key, or it's default value if key is not found
+    """
     # we rely on the fallback mechanism to get our predefined defaults
     # if no user config is found
-    return _config_parser.get(_MAIN_SECTION_NAME, key, fallback=_DEFAULTS[key])
+    return _CONFIG_PARSER.get(_MAIN_SECTION_NAME, key, fallback=_DEFAULTS[key])
 
 
 def _set(key, value):
-    # we only store the value if it differs from default or already stored one
+    """
+    Sets the value of a config entry identified by a key *only* when value differs from default or stored one
+
+    :param key: config entry key
+    :type key: str
+    :param value: config entry value
+    :type value: any
+    """
     if value != _get(key):
-        _config_parser.set(_MAIN_SECTION_NAME, key, value)
+        _CONFIG_PARSER.set(_MAIN_SECTION_NAME, key, value)
 
 
 # ConfigParser.read won't raise an exception if read fails because of missing file
@@ -136,38 +225,38 @@ def _set(key, value):
 # get an "empty" config
 # if config file is invalid, we raise a ValueError with details
 try:
-    _config_parser.read(_CONFIG_FILE_PATH)
-except DuplicateOptionError as e:
-    raise ValueError(e)
-except ParsingError as e:
-    raise ValueError(e)
+    _CONFIG_PARSER.read(_CONFIG_FILE_PATH)
+except DuplicateOptionError as duplicate_error:
+    raise ValueError(duplicate_error)
+except ParsingError as parsing_error:
+    raise ValueError(parsing_error)
 
 # init logging system
 logging.basicConfig(level=_LOG_LEVELS[_get(_LOG_LEVEL)],
                     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
                     stream=sys.stdout)
-_logger = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 
 # in here, we maintain a list of third party loggers for which we don't want to see anything but WARNING & up
-third_party_log_polluters = [
+_THIRD_PARTY_LOG_POLLUTERS = [
     'watchdog.observers.inotify_buffer',
 ]
-for third_party_log_polluter in third_party_log_polluters:
+
+for third_party_log_polluter in _THIRD_PARTY_LOG_POLLUTERS:
     logging.getLogger(third_party_log_polluter).setLevel(logging.WARNING)
 
 # add our main section if not already present (i.e. previous read failed)
-if not _config_parser.has_section(_MAIN_SECTION_NAME):
-    _logger.debug('adding main section to config')
-    _config_parser.add_section(_MAIN_SECTION_NAME)
+if not _CONFIG_PARSER.has_section(_MAIN_SECTION_NAME):
+    _LOGGER.debug('adding main section to config')
+    _CONFIG_PARSER.add_section(_MAIN_SECTION_NAME)
 
 # cleanup unused options
-for option in _config_parser.options(_MAIN_SECTION_NAME):
+for option in _CONFIG_PARSER.options(_MAIN_SECTION_NAME):
     if option not in _DEFAULTS.keys():
-        _logger.debug("Removed obsolete config option : '%s'", option)
-        _config_parser.remove_option(_MAIN_SECTION_NAME, option)
+        _LOGGER.debug("Removed obsolete config option : '%s'", option)
+        _CONFIG_PARSER.remove_option(_MAIN_SECTION_NAME, option)
 
-
-_logger.debug("User config file dump - START")
-for option in _config_parser.options(_MAIN_SECTION_NAME):
-    _logger.debug("%s = %s", option, _get(option))
-_logger.debug("User config file dump - END")
+_LOGGER.debug("User config file dump - START")
+for option in _CONFIG_PARSER.options(_MAIN_SECTION_NAME):
+    _LOGGER.debug("%s = %s", option, _get(option))
+_LOGGER.debug("User config file dump - END")
