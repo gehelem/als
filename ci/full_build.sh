a30ef0e41b1b66ddadf057bd90f0beae237d854e
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2019-09-12 16:33:05 +0200

    Try and parallelize Travis Jobs
    Part of Issue #61

--- ci/full_build.sh
+++ ci/full_build.sh
@@ -4,38 +4,9 @@
 #
 ###########################################################################
 
-# check pylint on als package and return nonzero only in these cases :
-#
-#  - pylint encountered a fatal error
-#  - pylint found errors in the code
-#  - pylint was incorrectly invoked
-
-
-echo "**********************************************************************"
-echo "******** pylint analysis START"
-echo "**********************************************************************"
-
-pylint src/als
-rc=$?
-
-FATAL_ERROR=1
-ERRORS_FOUND=2
-USAGE_ERROR=32
-
-if [[ $((${rc} & ${FATAL_ERROR})) -ne 0 || $((${rc} & ${ERRORS_FOUND}))  -ne 0 || $((${rc} & ${USAGE_ERROR})) -ne 0 ]]
-then
-    echo "**********************************************************************"
-    echo "******** pylint analysis FAILED"
-    echo "**********************************************************************"
-    exit 1
-fi
-
-echo "**********************************************************************"
-echo "******** pylint analysis OK"
-echo "**********************************************************************"
+$(readlink -f $(dirname $(readlink -f ${0})))/pylint.sh
 
 # run Tests
-
 echo "**********************************************************************"
 echo "******** Tests START"
 echo "**********************************************************************"
