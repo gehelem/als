3b8817561f3c49692b1e2109425a1ca2b1aa828c
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2019-09-24 15:56:59 +0200

    start with blueprint for input module

---
+++ src/als/io/input.py
@@ -0,0 +1,47 @@
+"""
+Provides everything need to handle ALS main inputs : images.
+
+We need to read file and in the future, get images from INDI
+"""
+import logging
+from abc import abstractmethod
+
+from PyQt5.QtCore import QObject
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class InputListener(QObject):
+    """
+    In charge of input management, **abstract class**
+    """
+    pass
+
+    @abstractmethod
+    def create_listener(self, listener_type):
+        """
+        Creates specialized input listeners.
+
+        :param listener_type: what type of listener to create
+        :type listener_type: str : allowed values :
+
+          - 'FS' to create a filesystem listener
+
+        :return: an input listener
+        :rtype: FileSystemListener
+        """
+        pass
+
+
+class FileSystemListener(InputListener):
+    """
+    Watches file changes (creation, move) in a specific filesystem folder
+    """
+    pass
+
+
+class ImageFileReader(QObject):
+    """
+    Encapsulates everything needed to read image files from disk
+    """
+    pass
