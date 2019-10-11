5694c3d7df999812931b612139e248ca488c3dec
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2019-10-11 17:58:12 +0200

    review views module

--- src/als/ui/views.py
+++ src/als/ui/views.py
@@ -8,19 +8,20 @@ from PyQt5.QtWidgets import QGraphicsView, QWidget
 
 
 class ImageView(QGraphicsView):
-
-    _ZOOM_SCALE_RATIO = 1.1
-
     """
     The main image view.
 
     Subclasses QGraphicsView to add mousewheel zoom features
     """
+
+    _ZOOM_SCALE_RATIO = 1.1
+
     def __init__(self, parent: typing.Optional[QWidget] = ...):
         super().__init__(parent)
         self.setDragMode(QGraphicsView.ScrollHandDrag)
         self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
 
+    # pylint: disable=C0103
     def wheelEvent(self, event: QtGui.QWheelEvent) -> None:
         """
         Performs zoom in & out according to mousewheel moves
