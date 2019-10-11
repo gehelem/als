b4cfab5d3dc809003332e256761ace43d3e9d308
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2019-10-11 14:28:34 +0200

    Add doc to als.ui.views module

--- src/als/ui/views.py
+++ src/als/ui/views.py
@@ -1,3 +1,6 @@
+"""
+Provide various views used in the GUI
+"""
 import typing
 
 from PyQt5 import QtGui
@@ -5,11 +8,21 @@ from PyQt5.QtWidgets import QGraphicsView, QWidget
 
 
 class ImageView(QGraphicsView):
-    def __init__(self, parent: typing.Optional[QWidget] = ...) -> None:
+    """
+    The main image view.
+
+    Subclasses QGraphicsView to add mousewheel zoom features
+    """
+    def __init__(self, parent: typing.Optional[QWidget] = ...):
         super().__init__(parent)
 
     def wheelEvent(self, event: QtGui.QWheelEvent) -> None:
+        """
+        Performs zoom in & out according to mousewheel moves
 
+        :param event: The Qt wheel event
+        :type event: QtGui.QWheelEvent
+        """
         self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
         self.setDragMode(QGraphicsView.ScrollHandDrag)
 
