a47b1c4ccec4726b6211c9f7162325ecb96a2795
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2020-04-16 12:48:06 +0200

    remove dead file
    to check out old code, Git history is the way

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,35 +1,6 @@
-import numpy as np
-from pytest import raises
-
-from als.legacy_code_stash import get_limit_and_utype
+# TODO
 
 
-def test_get_limit_and_utype_invalid():
-    # the use of pytest.raises context manager allows to
-    # that an expected exception is actually thrown
-    with raises(ValueError):
-        _, _ = get_limit_and_utype(np.array([0, 0, 0], dtype=np.float16))
+def test_todo():
 
-
-def test_get_limit_and_utype_8bits():
-    image = np.array([0, ], dtype=np.uint8)
-
-    limit, type_string = get_limit_and_utype(image)
-
-    true_limit = 2. ** 8 - 1
-    true_type_string = "uint8"
-
-    assert true_limit == limit
-    assert true_type_string == type_string
-
-
-def test_get_limit_and_utype_16bits():
-    image = np.array([0, ], dtype=np.uint16)
-
-    limit, type_string = get_limit_and_utype(image)
-
-    true_limit = 2. ** 16 - 1
-    true_type_string = "uint16"
-
-    assert true_limit == limit
-    assert true_type_string == type_string
+    assert True
