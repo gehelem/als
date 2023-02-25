5c2afe1dc1a3d98473d5e6cb599b474ae2915cc7
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2023-02-25 23:35:42 +0100

    switch generated HTML doc to wide format

--- docs/conf.py
+++ docs/conf.py
@@ -135,12 +135,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "groundwork"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
+html_theme_options = {
+
+    'prev_next_buttons_location': 'both',
+    'style_external_links': True,
+
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -170,6 +175,10 @@ else:
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
