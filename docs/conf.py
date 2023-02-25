5b32974f7c7e1a53bf3eb9f59990f421055f523f
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2023-02-25 21:27:27 +0100

    switch generated html doc theme to dark & wide

--- docs/conf.py
+++ docs/conf.py
@@ -135,17 +135,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "groundwork"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-
-    'prev_next_buttons_location': 'both',
-    'style_external_links': True,
-
-}
+html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
